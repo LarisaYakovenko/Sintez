30f486b4660de766bd678bb0c32616eed067006b
LarisaYakovenko <133209659+LarisaYakovenko@users.noreply.github.com>
2024-04-23 21:02:54 +0300

    Update ServisListItem.jsx

--- src/components/ServisListItem/ServisListItem.jsx
+++ src/components/ServisListItem/ServisListItem.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Cart, Img, P, Description } from './ServisListItem.styled';
-import images from '../../images/cafe.png';
+// import images from '../../images/cafe.png';
 
 const ServisListItem = ({ image, title, description }) => {
   return (
     <Cart>
-      <Img src={images} alt="tehnik" width="300" />
-      {/* <Img src={`../../images/${image}`} alt="tehnik" width="300" /> */}
+      {/* <Img src={images} alt="tehnik" width="300" /> */}
+      <Img src={`../../images/${image}`} alt="tehnik" width="300" />
       <P>{title}</P>
       <Description>{description}</Description>
     </Cart>
