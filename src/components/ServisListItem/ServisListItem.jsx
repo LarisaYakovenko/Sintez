183ea827322ada5548f617b9f9c13f89f4c65cd2
LarisaYakovenko <133209659+LarisaYakovenko@users.noreply.github.com>
2024-04-24 14:38:34 +0300

    Update ServisListItem.jsx

--- src/components/ServisListItem/ServisListItem.jsx
+++ src/components/ServisListItem/ServisListItem.jsx
@@ -6,7 +6,7 @@ const ServisListItem = ({ image, title, description }) => {
   return (
     <Cart>
       {/* <Img src={images} alt="tehnik" width="300" /> */}
-      <Img src={`../../images/${image}`} alt="tehnik" width="300" />
+      <Img src={require(`../../images/${image}`)} alt="tehnik" width="300" />
       <P>{title}</P>
       <Description>{description}</Description>
     </Cart>
